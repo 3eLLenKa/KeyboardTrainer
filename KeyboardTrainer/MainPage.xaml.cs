75e522d35a953bdcefd85582f0812745de7e9f5d
Дмитрий Бейкун <alexkomkov39@gmail.com>
2023-10-30 21:38:31 +0200

    commit all

--- KeyboardTrainer/MainPage.xaml.cs
+++ KeyboardTrainer/MainPage.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Timers;
 using System.Diagnostics;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace KeyboardTrainer
 {
--- KeyboardTrainer/MainWindow.xaml.cs
+++ KeyboardTrainer/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KeyboardTrainer
 {
@@ -39,7 +27,7 @@ namespace KeyboardTrainer
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Application.Current.Shutdown();
+            Application.Current.Shutdown(); 
         }
     }
 }
